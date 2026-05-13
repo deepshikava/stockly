fc0694d53c2b43c5456ab54811476a56c1703aec
JARVIS\deeps <deepshika2407@gmail.com>
2026-05-13 10:18:36 +0800

    Adding Header and the dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 "use client";
 
+import { Card } from "@/components/ui/card";
+import AppHeader from "./AppHeader/AppHeader";
+
 export default function Home() {
-  return <div>This is Home Page</div>;
+  return (
+    <div className="p-3">
+      <Card className="flex flex-col shadow-none p-2">
+        <AppHeader />
+      </Card>
+    </div>
+  );
 }
